65ea82345d3dff03816ca6a3df699637628db52f
zudha_prtm <zudhapratama123@gmail.com>
2024-01-15 10:48:44 +0700

    fix

--- app/Http/Controllers/Pembelian/PesananPembelianController.php
+++ app/Http/Controllers/Pembelian/PesananPembelianController.php
@@ -532,7 +532,7 @@ class PesananPembelianController extends Controller
         $pesananpembeliandetail = PesananPembelianDetail::with('products.merks')            
             ->where('pesanan_pembelian_id', '=', $pesananpembelian->id)->get();
         $jmlBaris  = $pesananpembeliandetail->count();
-        $perBaris = 10;
+        $perBaris = 9;
         $totalPage = ceil($jmlBaris / $perBaris);
         // dd($totalPage);
        
