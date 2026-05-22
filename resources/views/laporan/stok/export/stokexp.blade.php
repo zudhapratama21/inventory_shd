ea140a271ddc0ad7f1de11f34474bc1a8c19ccc4
zudha_prtm <zudhapratama123@gmail.com>
2026-05-22 15:07:37 +0700

    fix

--- resources/views/laporan/stok/export/stokexp.blade.php
+++ resources/views/laporan/stok/export/stokexp.blade.php
@@ -1,44 +1,33 @@
-<table >
+<table>
     <thead>
         <tr>
-            <th>No</th>               
+            <th>No</th>
             <th>Nama Produk</th>
-            <th>Kode Barang</th>            
-            <th>Tgl Expired</th>           
+            <th>Kode Barang</th>
+            <th>Tgl Expired</th>
             <th>Lot</th>
-            <th>Stok</th>
-            <th>Supplier</th>
-            <th>Harga Beli</th>
-            <th>Diskon (%) Beli</th>
-            <th>Diskon (Rp.) Beli</th>            
+            <th>Stok</th>           
         </tr>
     </thead>
     <tbody>
         @php
-            $no=1;
+            $no = 1;
         @endphp
         @foreach ($stokexp as $item)
-           
-                @foreach ($item->stokExp as $exp)
-                    <tr>
-                        <td>{{$no++}}</td>                                  
-                        <td>{{$item->nama}}</td>
-                        <td>{{$item->kode}}</td>                                 
-                        <td>
-                            @if ($item->status_exp == 1)
-                                {{ Carbon\Carbon::parse($item->tanggal)->format('d/m/Y') }}
-                            @else
-                                -
-                            @endif                            
-                        </td>                                                   
-                        <td>{{$exp->lot}}</td>
-                        <td>{{$exp->qty}}</td>
-                        <td>{{$exp->supplier ? $exp->supplier->nama : '-' }}</td>                                                                                  
-                        <td>{{$exp->harga_beli ? $exp->harga_beli : '-' }}</td>
-                        <td>{{$exp->diskon_persen ? $exp->diskon_persen : '-' }}</td>
-                        <td>{{$exp->diskon_rupiah ? $exp->diskon_rupiah : '-'}}</td>
-                    </tr>    
-                @endforeach                              
+            <tr>
+                <td>{{ $no++ }}</td>
+                <td>{{ $item->nama_produk }}</td>
+                <td>{{ $item->kode_item }}</td>
+                <td>
+                    @if ($item->status_exp == 1)
+                        {{ Carbon\Carbon::parse($item->tanggal)->format('d/m/Y') }}
+                    @else
+                        -
+                    @endif
+                </td>
+                <td>{{ $item->lot }}</td>
+                <td>{{ $item->qty }}</td>
+            </tr>
         @endforeach
     </tbody>
 </table>
